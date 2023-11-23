a70a7a93b190a73feca2985e1599e5aea7244228
Sairaj Khodwe <87792968+ETHICALGAMER1402@users.noreply.github.com>
2023-11-23 23:21:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request,url_for,redirect
 import smtplib
-import requests
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
